df37888438107bd5e63d5c2b4db6d73ef8e513ff
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-05 16:10:16 +0530

    Update signUP html and js file
    This modifies code to display proper error message

--- signUp.js
+++ signUp.js
@@ -5,12 +5,36 @@ function goToHomePage() {
   document.location.href = "index.html";
 }
 
+function isValidName(name) {
+  let nameArray = Array.from(name);
+  let filterdArray = nameArray.filter((charcter) => {
+    return (
+      typeof charcter === "string" &&
+      ((charcter >= "a" && charcter <= "z") ||
+        (charcter >= "A" && charcter <= "Z") ||
+        charcter === " ")
+    );
+  });
+  return nameArray.length === filterdArray.length;
+}
+let messageElement = document.createElement("p");
+messageElement.classList.add("message");
+
+function createMessage(element, messageTxt) {
+  messageElement.innerText = messageTxt;
+
+  element.nextElementSibling.before(messageElement);
+  return messageElement;
+}
+
 //After form submission
 
 let form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  messageElement.style.display = "none";
+
   console.log(event.target);
 
   let firstName = document.querySelector("#first-name").value;
@@ -19,43 +43,40 @@ form.addEventListener("submit", (event) => {
   let password = document.querySelector("#password").value;
   let repeatePassword = document.querySelector("#repeat-password").value;
   let checkbox = document.querySelector("#checkbox").checked;
-  let message = document.querySelector("#message");
-
-  message.style.display = "none";
-
-  function isValidName(name) {
-    let nameArray = Array.from(name);
-    let filterdArray = nameArray.filter((charcter) => {
-      return (
-        typeof charcter === "string" &&
-        ((charcter >= "a" && charcter <= "z") ||
-          (charcter >= "A" && charcter <= "Z") ||
-          charcter === " ")
-      );
-    });
-    return nameArray.length === filterdArray.length;
-  }
 
   if (!isValidName(firstName)) {
-    message.innerText = "Please provide valid First Name";
+    let firstNameElement = document.querySelector("#first-name");
+    let message = createMessage(firstNameElement, "!Provide valid first name");
     message.style.display = "block";
   } else if (!isValidName(lastName)) {
-    message.innerText = "Please provide valid Last Name";
+    let lastNameElement = document.querySelector("#last-name");
+    let message = createMessage(lastNameElement, "!Provide valid last name");
     message.style.display = "block";
-  } else if (password.length < 6) {
-    message.innerText = "Password length should be greater than 5";
+  } else if (password.length < 8) {
+    let passwordElement = document.querySelector("#password");
+    let message = createMessage(passwordElement, "!Minimum length should be 8");
     message.style.display = "block";
   } else if (password !== repeatePassword) {
-    message.innerText = "Password should Match";
+    let repeatePasswordElement = document.querySelector("#repeat-password");
+    let message = createMessage(
+      repeatePasswordElement,
+      "!Password is not matching"
+    );
     message.style.display = "block";
   } else if (!checkbox) {
-    message.innerText = "You must agree to terms and conditions";
+    let checkboxElement = document.querySelector(".checkbox");
+    console.log(checkboxElement);
+    let message = createMessage(
+      checkboxElement,
+      "!You must agree to terms and conditions"
+    );
+    console.log(form);
     message.style.display = "block";
   } else {
+    let successMessage = document.querySelector("#successMessage");
+    console.log(successMessage);
     form.reset();
-    message.innerText = "Regestered Successfully!";
-    message.style.display = "block";
-    message.style.color = "green";
-    message.style.border = "1px solid green";
+    successMessage.innerText = "Registered Successfully!";
+    successMessage.style.display = "block";
   }
 });
